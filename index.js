8c4380b44935635f6ea99e6935b4cbe528e4738e
TS000 <tylersnell@me.com>
2018-04-21 12:13:13 -0600

    added middleware entry point to index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function (cb) {
-  cb(null, 'Hello World');
-}
+var Express = require('express');
+var Webtask = require('webtask-tools');
+var bodyParser = require('body-parser')
+var app = Express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// todo middlewares
+app.use(require('./middlewares/db').connectDisconnect);
+require('./routes/stories')(app);
+
+module.exports = Webtask.fromExpress(app);
